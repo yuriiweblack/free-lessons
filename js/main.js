73254ef97f1f70fa6f3c88a4e0bd12b6abf91500
Yurii Black <95044145+yuriiweblack@users.noreply.github.com>
2022-07-27 19:24:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+ $('.menu__btn, .menu a').on('click', function () {
+
+  $('.menu__list').toggleClass('menu__list--active');
+  // $('.header__top-inner').toggleClass('header__top-inner--active');
+
+ });
+
+});
+
+// .menu a
